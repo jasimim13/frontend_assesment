c0cad332661f051ec0123421a071e839be170f90
jasimim13 <jasimimran786@icloud.com>
2023-09-12 14:20:31 +0500

    Updated Dumy API

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,35 +5,43 @@ export const getComments = async () => {
       body: "I was very glad to have you after such a long time. Can you plan a meetup? Maybe this weekend?",
       username: "Maria",
       userId: "1",
-      profile: require('../images/user1.png'),
+      profile: require("../images/user1.png"),
       parentId: null,
+      isAddedByUser: false,
     },
     {
       id: "2",
       body: "Home sweet home! I’m glad you are back. It’s been two year and miss the football matches we have together. A lot has been changed since you left. Let’s meet at the ground tomorrow evening?",
       username: "Alex Benjamin",
       userId: "2",
-      profile: require('../images/user2.png'),
+      profile: require("../images/user2.png"),
       parentId: null,
+      isAddedByUser: false,
     },
     {
       id: "3",
       body: "I was very glad to have you after such a long time. Can you plan a meetup? Maybe this weekend?",
       username: "Tania",
       userId: "3",
-      profile: require('../images/user3.png'),
+      profile: require("../images/user3.png"),
       parentId: null,
+      isAddedByUser: false,
     },
   ];
 };
 
 export const createComment = async (text, parentId = null) => {
-    return {
-      id: Math.random().toString(36).substr(2, 9),
-      body: text,
-      parentId,
-      userId: "1",
-      profile: require('../images/user4.png'),
-      username: "John Doe",
-    };
+  return {
+    id: Math.random().toString(36).substr(2, 9),
+    body: text,
+    parentId,
+    userId: "4",
+    profile: require("../images/user4.png"),
+    username: "John Doe",
+    isAddedByUser: true
+  };
+};
+
+export const deleteComment = async () => {
+    return {};
   };
